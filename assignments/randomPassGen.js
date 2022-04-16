398308b68454f83052df3a9338809cf916910c25
G_ibrahim_k <gulamibrahimkhan@gmail.com>
2022-04-17 02:54:32 +0500

    Revert "Revert "assignments,express basic websit,self learningCode""
    This reverts commit b905ec62e543a3a947596469d826eca1a0c1091f.

---
+++ assignments/randomPassGen.js
@@ -0,0 +1,18 @@
+
+
+var password = ""
+var length = 10
+
+for(i = 0; i < length; i++){
+    var random = Math.random()
+    var floor = Math.floor(random*10)
+    password += floor
+
+}
+
+console.log(password);
+
+x = new Date
+y = x.getDays()
+console.log(y);log
+log
